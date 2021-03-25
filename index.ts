5dec024c3766d9274487857e90177b0bc04dfd01
Navaneeth <vnavaneeth15@gmail.com>
2021-03-25 16:15:13 +0530

    test: test the cli

--- index.ts
+++ index.ts
@@ -1,40 +1,25 @@
 #!/usr/bin/env node
 import fs from 'fs';
-import chalk from 'chalk';
 import { Command } from 'commander';
 import path from 'path';
-import { Config, isConfig } from './src/util/config';
+import { Config, getConfig, validateConfig } from './src/util/config';
 import { generateNavBar } from './src/components/NavBar';
 import { genFiles } from './src/util/generateComponents';
+import chalk from 'chalk';
 
 // const PATH = __dirname + '/essential.config.json';
 const PATH = process.cwd() + '/essential.config.json';
 console.log(PATH);
-// console.log(process.cwd());
 const USE_TS = false;
-
-if (!fs.existsSync(PATH)) {
-	//file doesnt exist
-	console.log(
-		chalk.red(
-			'No essential.config.json found. Do you want to create one? (Y/n)' // TODO: have to create empty json file
-		)
-	);
-	process.exit(1);
+let config: Config;
+try {
+    config = getConfig(PATH);
+    config = validateConfig(config);
+} catch (err) {
+    // throw err;
+    console.error(err.message);
+    process.exit(1);
 }
-
-const file = fs.readFileSync(PATH, 'utf-8');
-const config = JSON.parse(file);
-
-if (!isConfig(config)) {
-	console.log(
-		chalk.red(
-			'Config is wrong. JSON file should contain the following keys: componentDir, stylesDir'
-		)
-	);
-	process.exit(1);
-}
-
 // config is safe now
 console.log(config);
 
@@ -46,12 +31,12 @@ const relativePathToCSS = path.relative(componentDir, stylesDir);
 const program = new Command();
 
 program
-	.option('-n, --name <type>', 'name of the component')
-	.requiredOption(
-		'-t, --component-type <type>',
-		'Type of component to generate'
-	)
-	.option('-ts, --typescript', 'Use Typescript', false);
+    .option('-n, --name <type>', 'name of the component')
+    .requiredOption(
+        '-t, --component-type <type>',
+        'Type of component to generate'
+    )
+    .option('-ts, --typescript', 'Use Typescript', false);
 
 program.parse(process.argv);
 
@@ -60,16 +45,19 @@ const options = program.opts();
 console.log(options);
 
 switch (options.componentType) {
-	case 'NavBar':
-		// generateNavBar(options.typescript, relativePathToCSS, options.name);
-		generateNavBar({
-			cssPath: relativePathToCSS,
-			name: options.name ? options.name : options.componentType,
-			useTS: options.typescript,
-			config,
-			genFiles,
-		});
-		break;
+    case 'NavBar':
+        // generateNavBar(options.typescript, relativePathToCSS, options.name);
+        generateNavBar({
+            cssPath: relativePathToCSS,
+            name: options.name ? options.name : options.componentType,
+            useTS: options.typescript,
+            config,
+            genFiles,
+        });
+        break;
+    default:
+        console.error(chalk.red('Invalid component type'));
+        process.exit(1);
 }
 
 // if (options.debug) console.log(options);
--- src/util/config.ts
+++ src/util/config.ts
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
 export interface Config {
     componentDir: string;
     stylesDir: string;
@@ -5,4 +8,39 @@ export interface Config {
 
 export function isConfig(object: any): object is Config {
     return 'componentDir' in object && 'stylesDir' in object;
+}
+
+export function getConfig(PATH: string): Config {
+    if (!fs.existsSync(PATH)) {
+        //file doesnt exist
+        console.error(
+            chalk.red(
+                'No essential.config.json found. Do you want to create one? (Y/n)' // TODO: have to create empty json file
+            )
+        );
+        throw new Error('No essential.config.json found');
+        // process.exit(1);
+    }
+
+    const file = fs.readFileSync(PATH, 'utf-8');
+    const config = JSON.parse(file);
+
+    if (!isConfig(config)) {
+        console.error(
+            chalk.red(
+                'Config is wrong. JSON file should contain the following keys: componentDir, stylesDir'
+            )
+        );
+        // process.exit(1);
+    }
+
+    return config;
+}
+
+export function validateConfig(config: Config) {
+    if (fs.existsSync(config.componentDir) && fs.existsSync(config.stylesDir)) {
+        return config;
+    } else {
+        throw new Error('Invalid Config. Directory does not exist');
+    }
 }
